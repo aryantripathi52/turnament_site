442a009ea8025c37005f2297338ee3af17452c18
SPY EYE OFFICIAL <tripathiaryan346@gmail.com>
2026-01-01 18:25:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2393271290-3a0b7",
+  "appId": "1:1049495192350:web:3f16cdabb5cf436b7b7f0f",
+  "apiKey": "AIzaSyA8709dwtbznW6g7NQ-Es9z-s2uNFTw2Lc",
+  "authDomain": "studio-2393271290-3a0b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1049495192350"
+};
